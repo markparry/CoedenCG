06860a607b942536b3db6adc8b7454ba3eebb50f
antgwms <anthony.gwill@gmail.com>
2016-01-05 11:28:47 +0000

    flare.js
    flare.js holds the departmental information for parent and children

---
+++ flare.js
@@ -0,0 +1,86 @@
+{
+    "name": "Cefnogaeth Gorfforaethol",
+    "children": [{
+        "name": "Gwasanaeth 1",
+        "children": [{
+            "name": "tim1",
+            "children": [{
+                "name": "T1enw1",
+                "size": 3938
+            }, {
+                "name": "T1enw2",
+                "size": 3812
+            }, {
+                "name": "T1enw3",
+                "size": 6714
+            }, {
+                "name": "T1enw4",
+                "size": 743
+            }]
+        }, {
+            "name": "tim2",
+            "children": [{
+                "name": "T2enw1",
+                "size": 3534
+            }, {
+                "name": "T2enw2",
+                "size": 5731
+            }, {
+                "name": "T2enw3",
+                "size": 7840
+            }, {
+                "name": "T2enw4",
+                "size": 5914
+            }, {
+                "name": "T2enw5",
+                "size": 3416
+            }]
+        }, {
+            "name": "tim3",
+            "children": [{
+                "name": "T3enw1",
+                "size": 7074
+            }]
+        }]
+    }, {
+        "name": "Gwasanaeth 2",
+        "children": [{
+            "name": "tim4",
+            "size": 17010
+        }, {
+            "name": "tim5",
+            "size": 5842
+        }, {
+            "name": "tim6",
+            "children": [{
+                "name": "T5enw1",
+                "size": 1983
+            }, {
+                "name": "T5enw2",
+                "size": 2047
+            }, {
+                "name": "T5enw3",
+                "size": 1375
+            }, {
+                "name": "T5enw4",
+                "size": 8746
+            }, {
+                "name": "T5enw5",
+                "size": 2202
+            }, {
+                "name": "T5enw6",
+                "size": 1382
+            }, {
+                "name": "T5enw7",
+                "size": 1629
+            }, {
+                "name": "T5enw8",
+                "size": 1675
+            }, {
+                "name": "T5enw9",
+                "size": 2042
+            }]
+ 
+        }]
+    }]
+}
